c751740ca2891767c010879d5c6b4b2a4db4be46
yuriiup <kaktus20110@yandex.com>
2025-10-19 14:27:32 +0300

    Add files via upload
    last fixed

--- histogram.py
+++ histogram.py
@@ -16,11 +16,11 @@ def histogram(average_diameter, dist_earth):
     plt.figure(figsize=(14, 10))
     plt.hist(df["Average diameter"], color="limegreen", edgecolor="black")
 
-    plt.xlabel("Average diameter")  # ось X
-    plt.ylabel("Dist to Earth")  # ось Y
-    plt.title("Hаспределение астероидов по размерам")
+    plt.xlabel("Average diameter")
+    plt.ylabel("Dist to Earth")
+    plt.title("Distribution of asteroids by size")
 
-    plt.grid(axis="y", linestyle="--", alpha=1)  # сетка
+    plt.grid(axis="y", linestyle="--", alpha=1)
     plt.savefig('static/histogram.png')
 
 histogram(_average_diameter(), _dist_earth())
